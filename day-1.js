214c374674fc3bd3248df4e699d7ad86c3dc4100
Kim Ewing <pekar.kimberly@gmail.com>
2024-12-07 11:05:49 -0700

    adds solution for part 2

--- day-1.js
+++ day-1.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const data = fs.readFileSync('day-01.input.txt', 'utf8');
 const lines = data.split(/\r?\n/).map(line => line.split(/\s+/));
 
-function divideColumnA(lines){
+function divideColumnA(lines) {
     return lines.map(line => parseInt(line[0]))
 }
 
@@ -22,20 +22,30 @@ let sortedColumnB = sortColumn(columnB)
 function calculateDistances(sortedColumnA, sortedColumnB) {
     let distances = 0
     for (let i = 0; i < sortedColumnA.length; i++) {
-        distances +=  Math.abs(sortedColumnA[i] - sortedColumnB[i])
-     }
+        distances += Math.abs(sortedColumnA[i] - sortedColumnB[i])
+    }
     return distances
 }
 
-console.log(calculateDistances(sortedColumnA, sortedColumnB))
+calculateDistances(sortedColumnA, sortedColumnB)
 
-// function calculateDistances(sortedColumnA, sortedColumnB) {
-//     let distances = []
-//     for (let i = 0; i < sortedColumnA.length; i++) {
-//         distances.push(Math.abs(sortedColumnA[i] - sortedColumnB[i]))
-//      }
-//     return distances.reduce((acc, number) => {
-//         acc += number
-//         return acc
-//     }, 0)
-// }
+// part 2
+
+function countOccurences(sortedColumnA, sortedColumnB) {
+    let occurences = []
+    sortedColumnA.forEach(columnANum => {
+       let count = sortedColumnB.reduce((acc, columnBNum) => {
+            if (columnANum === columnBNum) {
+                acc++
+            }
+        return acc
+        }, 0)
+        occurences.push(count)
+    })
+    let similarityScore = 0
+    for (let i = 0; i < sortedColumnA.length; i++) {
+       similarityScore +=  sortedColumnA[i] * occurences[i]
+    }
+    return similarityScore
+}
+console.log(countOccurences(sortedColumnA, sortedColumnB))
