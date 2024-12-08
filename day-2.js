ba5fa453f80eb37167b25838295fa80578e1f0bb
Kim Ewing <pekar.kimberly@gmail.com>
2024-12-08 15:37:19 -0700

    adds in progress removeNotIncreasing function

--- day-2.js
+++ day-2.js
@@ -60,4 +60,37 @@ function checkForSafety(data) {
     return finalArray.length
 }
 
-console.log(checkForSafety(integers))
+// console.log(checkForSafety(integers))
+
+// part 2
+
+// test cases
+let test1 = [1,2,3,4,5,5] //expected to return safe (remove one of the 5s in last two positions)
+let test2 = [1,3,2,5,3,7] //expected to return unsafe
+let test3 = [9,8,9,7,5,4,3] //expected to return safe (remove the 9 in [2] position)
+let test4 = [100,90,80,70] //expected to return unsafe
+let test5 = [999,7,6,5,4] //expected to return safe (remove the 999 in [0] position)
+let test6 = [999,1,5,10] //expected to return unsafe
+let test7 = [1,3,5,7,9,100] //expected to return safe (remove the 100 in last position)
+
+function removeNotIncreasing(array) {
+    let count = 0
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] >= array[i + 1]) {
+            count += 1
+        }
+    } 
+    if (count === 1) {
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] >= array[i + 1]) {
+                array.splice(i, 1)
+                break
+            }
+        }
+    } 
+    return array
+}
+console.log("test1:", removeNotIncreasing(test1))
+console.log("test2:", removeNotIncreasing(test2))
+console.log("test6:", removeNotIncreasing(test6))
+console.log("test7:", removeNotIncreasing(test7))
